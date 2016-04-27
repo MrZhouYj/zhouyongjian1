041e321b18c8100071d0a83910ee4c2b21834891
zhouyongjian <MrZhouYongJian@163.com>
2016-04-27 14:42:26 +0800

    sdcsd

---
+++ zhou/doc/html/search/all_a.js
@@ -0,0 +1,21 @@
+var searchData=
+[
+  ['latestmessage',['latestMessage',['../interface_e_m_conversation.html#a21a7bd7077a0bdffcf81fe3f282fe416',1,'EMConversation']]],
+  ['latestmessagefromothers',['latestMessageFromOthers',['../interface_e_m_conversation.html#aedadc24a089c35ceba41bd87a8eb2f98',1,'EMConversation']]],
+  ['latitude',['latitude',['../interface_e_m_location_message_body.html#a250f22dfc88010c5243ba0f2f389d0e2',1,'EMLocationMessageBody']]],
+  ['leavechatroom_3aerror_3a',['leaveChatroom:error:',['../protocol_i_e_m_chatroom_manager-p.html#a7b8a4a9d3627a3070569b50ba78125e7',1,'IEMChatroomManager-p']]],
+  ['leavegroup_3aerror_3a',['leaveGroup:error:',['../protocol_i_e_m_group_manager-p.html#a7ddf033ceaa8bbe34d53f6904f22ab5f',1,'IEMGroupManager-p']]],
+  ['list',['list',['../interface_e_m_cursor_result.html#abf5113042bfaf11657f1fe8ad7120cb9',1,'EMCursorResult']]],
+  ['loadallconversationsfromdb',['loadAllConversationsFromDB',['../protocol_i_e_m_chat_manager-p.html#aa7f112da8fe1ee49c9749c417e1186cf',1,'IEMChatManager-p']]],
+  ['loadallmygroupsfromdb',['loadAllMyGroupsFromDB',['../protocol_i_e_m_group_manager-p.html#ad32a1e1830d5d858b9d9c7eae31fc1fe',1,'IEMGroupManager-p']]],
+  ['loadmessagewithid_3a',['loadMessageWithId:',['../interface_e_m_conversation.html#acb5a2ad74dda943f56f7da4585f2a1a5',1,'EMConversation']]],
+  ['loadmoremessagescontain_3abefore_3alimit_3a',['loadMoreMessagesContain:before:limit:',['../interface_e_m_conversation.html#add07b6ce7d0ba39c5afd2771d963b618',1,'EMConversation']]],
+  ['loadmoremessagesfromid_3alimit_3a',['loadMoreMessagesFromId:limit:',['../interface_e_m_conversation.html#a3815dafb0dd54337a581132419cdde2c',1,'EMConversation']]],
+  ['loadmoremessageswithtype_3abefore_3alimit_3a',['loadMoreMessagesWithType:before:limit:',['../interface_e_m_conversation.html#a60e02ababff7f6e8816a633d2db42de5',1,'EMConversation']]],
+  ['localpath',['localPath',['../interface_e_m_file_message_body.html#aa109e9466c919f0fe5c1c9ef37b5386d',1,'EMFileMessageBody']]],
+  ['localview',['localView',['../interface_e_m_call_session.html#a8717ba20f2e040ff590725c28231fb25',1,'EMCallSession']]],
+  ['loginwithusername_3apassword_3a',['loginWithUsername:password:',['../interface_e_m_client.html#a586c4bdeb649055886c25603125072fe',1,'EMClient']]],
+  ['loglevel',['logLevel',['../interface_e_m_options.html#a0532bf2756002f639f403bed4c8a589e',1,'EMOptions']]],
+  ['logout_3a',['logout:',['../interface_e_m_client.html#ab4562627674072e4b2a98ece2d429efb',1,'EMClient']]],
+  ['longitude',['longitude',['../interface_e_m_location_message_body.html#a803a3f6cd011398d9f3125092359ce67',1,'EMLocationMessageBody']]]
+];
---
+++ zhou/doc/html/search/functions_f.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['searchpublicgroupwithid_3aerror_3a',['searchPublicGroupWithId:error:',['../protocol_i_e_m_group_manager-p.html#a6bb1222214e0f15ce1034da4be30a55f',1,'IEMGroupManager-p']]],
+  ['setapnsnickname_3a',['setApnsNickname:',['../interface_e_m_client.html#a29f928aab2d104e6bf00426ed0c3d7dc',1,'EMClient']]],
+  ['sharedclient',['sharedClient',['../interface_e_m_client.html#abdf7f014fc5a6c76d9a9bd676d9543f5',1,'EMClient']]],
+  ['startvideorecord_3a',['startVideoRecord:',['../interface_e_m_call_session.html#a8be657b36ec9ed51e91d98aefcc522b3',1,'EMCallSession']]],
+  ['stopvideorecord',['stopVideoRecord',['../interface_e_m_call_session.html#af023d9a8c8163519ade3040ceb8abe90',1,'EMCallSession']]]
+];
